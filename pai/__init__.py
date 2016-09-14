054b74dfbe5078b59cfda569b00201e047fbe60b
Andrew Hawker <andrew.r.hawker@gmail.com>
2016-09-13 20:58:01 -0700

    Create pai package.

---
+++ pai/__init__.py
@@ -0,0 +1,9 @@
+"""
+    pai
+    ~~~
+
+    Language for describing resources/relations as shell-safe strings.
+
+    :copyright: (c) 2016 Andrew Hawker.
+    :license: Apache 2.0, see LICENSE for more details.
+"""
