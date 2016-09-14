401e34c6da01de04c4b6c230514593e2c010121e
Andrew Hawker <andrew.r.hawker@gmail.com>
2016-09-13 21:16:05 -0700

    Expose 'parser.parse' and 'parser.parse_gen' as public API calls.
    These two functions should be the _only_ things invoked by any consumer.
    If the require changes to the language, or runtime modifications, they should
    be implementing their own syntax and visitor implementations and invoking
    `paip` themselves.

--- pai/__init__.py
+++ pai/__init__.py
@@ -7,3 +7,14 @@
     :copyright: (c) 2016 Andrew Hawker.
     :license: Apache 2.0, see LICENSE for more details.
 """
+
+from . import parser
+from .parser import *
+from . import syntax
+from .syntax import *
+
+
+__all__ = parser.__all__ + syntax.__all__
+
+
+__version__ = '1.0.0'
